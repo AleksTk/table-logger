d2326e053fb972ed7ae4950d0e8c6e7c9f4399b8
Alexander Tkachenko <alex.tk.fb@gmail.com>
2017-10-09 00:12:49 +0300

    Fixed bug with wrong column width.

--- table_logger/table_logger.py
+++ table_logger/table_logger.py
@@ -212,17 +212,20 @@ class TableLogger(object):
             *args: row cells
         """
         formatters = []
-
+        col_offset = 0
         # initialize formatters for row-id, timestamp and time-diff columns
         if self.rownum:
             formatters.append(fmt.RowNumberFormatter.setup(0))
+            col_offset += 1
         if self.timestamp:
             formatters.append(fmt.DatetimeFormatter.setup(
                 datetime.datetime.now(),
                 fmt='{:%Y-%m-%d %H:%M:%S.%f}'.format,
                 col_width=26))
+            col_offset += 1
         if self.time_diff:
             formatters.append(fmt.TimeDeltaFormatter.setup(0))
+            col_offset += 1
 
         # initialize formatters for user-defined columns
         for coli, value in enumerate(args):
@@ -234,16 +237,16 @@ class TableLogger(object):
                 kwargs['col_width'] = self.default_colwidth
             if coli in self.column_widths:
                 kwargs['col_width'] = self.column_widths[coli]
-            elif self.columns and self.columns[coli] in self.column_widths:
-                kwargs['col_width'] = self.column_widths[self.columns[coli]]
+            elif self.columns and self.columns[coli + col_offset] in self.column_widths:
+                kwargs['col_width'] = self.column_widths[self.columns[coli + col_offset]]
 
             # set formatter function
             if fmt_class == fmt.FloatFormatter and self.float_format is not None:
                 kwargs['fmt'] = self.float_format
             if coli in self.column_formatters:
                 kwargs['fmt'] = self.column_formatters[coli]
-            elif self.columns and self.columns[coli] in self.column_formatters:
-                kwargs['fmt'] = self.column_formatters[self.columns[coli]]
+            elif self.columns and self.columns[coli + col_offset] in self.column_formatters:
+                kwargs['fmt'] = self.column_formatters[self.columns[coli + col_offset]]
 
             formatter = fmt_class.setup(value, **kwargs)
             formatters.append(formatter)
